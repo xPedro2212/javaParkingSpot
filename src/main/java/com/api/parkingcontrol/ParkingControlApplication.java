cf408c2765c5c7fdc7cb33cd5722429b111451b2
Pedro Aurelio <ecore.pedroaurelio@unifertil.com.br>
2025-07-30 16:40:39 -0300

    importing the API, pagination

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -1,13 +1,23 @@
-package com.api.parking_control;
+package com.api.parkingcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParkingControlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
 	}
+
+	@GetMapping("/")
+	public String index(){
+		return "olá mundo!";
+	}
+
+
 
 }
--- src/test/java/com/api/parkingcontrol/ParkingControlApplicationTests.java
+++ src/test/java/com/api/parkingcontrol/ParkingControlApplicationTests.java
@@ -1,4 +1,4 @@
-package com.api.parking_control;
+package com.api.parkingcontrol;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
